0440218956b01f98899a28f11809302e3d8595fe
Hybrid Dog <ovvv@web.de>
2017-09-02 11:38:38 +0200

    Remove item magnet behaviour
    Now only items inside radius 1 are picked up

--- init.lua
+++ init.lua
@@ -24,52 +24,6 @@ local function pickup_step()
 					end
 				end
 			end
-
-			for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 2)) do
-				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
-					if object:get_luaentity().collect then
-						if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-							local pos1 = pos
-							pos1.y = pos1.y+0.2
-							local pos2 = object:getpos()
-							local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
-							vec.x = vec.x*3
-							vec.y = vec.y*3
-							vec.z = vec.z*3
-							object:setvelocity(vec)
-							object:get_luaentity().physical_state = false
-							object:get_luaentity().object:set_properties({
-								physical = false
-							})
-
-							minetest.after(1, function(args)
-								local lua = object:get_luaentity()
-								if object == nil or lua == nil or lua.itemstring == nil then
-									return
-								end
-								if inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-									inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
-									if object:get_luaentity().itemstring ~= "" then
-										minetest.sound_play("item_drop_pickup", {
-											to_player = player:get_player_name(),
-											gain = 0.4,
-										})
-									end
-									object:get_luaentity().itemstring = ""
-									object:remove()
-								else
-									object:setvelocity({x=0,y=0,z=0})
-									object:get_luaentity().physical_state = true
-									object:get_luaentity().object:set_properties({
-										physical = true
-									})
-								end
-							end, {player, object})
-
-						end
-					end
-				end
-			end
 		end
 	end
 	minetest.after(0.1, pickup_step)
